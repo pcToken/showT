33252ca13499c1562d79864a4e73fa36b317b5ac
Sho Arata <arata.sho@gmail.com>
2017-08-22 19:31:08 -0400

    layout bug

--- src/Routes/Compras/contentCompras.js
+++ src/Routes/Compras/contentCompras.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import Listado from '../../Components/Compras/listado.js'
 import AnhadirCompra from "../../Components/Compras/anhadirCompra.js"
 export default (
-  <div>
+  <div style={{margin:0,padding:0,height:"100%"}}>
   <Route exact path="/compras/solicitar" component={Listado} />
   <Route exact path="/compras/solicitar/anhadir" component={AnhadirCompra}/>
   </div>
